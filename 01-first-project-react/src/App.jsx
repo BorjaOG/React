0cf7046e42eebc4fe148ed22d1655a5d48d9e51f
Borja Orge Gesteira <borja_1089@hotmail.com>
2023-05-15 01:28:57 +0800

    first react

---
+++ 01-first-project-react/src/App.jsx
@@ -0,0 +1,16 @@
+export function App(){
+  return(
+    <article style={{ display: 'flex'}}>
+      <header>
+        <img src="https://unavatar.io/borjaog" alt="avatar Borja OG" />
+        <div>
+          <strong>Borja OG</strong>
+          <span>@borjaog</span>
+        </div>
+      </header>
+      <aside>
+        <button>follow</button>
+      </aside>
+    </article>
+  )
+}
